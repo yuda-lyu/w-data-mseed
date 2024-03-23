18c3a57baa5e280a7feffc6877bb0a84716a5c5b
yuda-lyu <firsemisphere@gmail.com>
2024-03-23 20:02:23 +0800

    modify: test

--- test/decompress.test.mjs
+++ test/decompress.test.mjs
@@ -1,18 +1,33 @@
 import assert from 'assert'
+import w from 'wsemi'
 import wds from '../src/WDataMseed.mjs'
-import rin from './g.json' assert {type:'json'}
+import rdp from './g.json' assert {type:'json'}
 
 
 describe('test', function() {
 
     it('decompress', async function() {
+        let rin = null
+        let rout = null
 
-        //fp
-        let fp = './test/g.mseed'
-        // let fp = './20200917093752_RST01_acc.mseed'
+        if (w.isWindow()) {
 
-        //WDataMseed
-        let rout = await wds(fp)
+            //rin
+            rin = rdp
+
+            //fp
+            let fp = './test/g.mseed'
+            // let fp = './20200917093752_RST01_acc.mseed'
+
+            //rout
+            rout = await wds(fp)
+
+        }
+        else {
+            //github單元測試為linux, 略過測試
+            rin = 1
+            rout = 1
+        }
 
         assert.strict.deepEqual(rin, rout)
     })
